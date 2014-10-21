a3c6383e03712c05f642dc6fce36a12902fb8d40
youcune <you@nakanishi.email>
2014-10-21 17:56:20 +0900

    タイトル表示させるところまで

--- cococlip-ios/ViewController.swift
+++ cococlip-ios/ViewController.swift
@@ -7,19 +7,38 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var clips: [Clip] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        clips = CococlipService.getClipsBy(CLLocation(latitude: 22, longitude: 22))
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int  {
+        return clips.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath:NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+        cell.textLabel?.text = clips[indexPath.row].title!
+        return cell
+    }
+    
+    func tableView(tableView: UITableView?, didSelectRowAtIndexPath indexPath:NSIndexPath!) {
+        var text: String = clips[indexPath.row].title!
+        println(text)
+    }
 }
-
